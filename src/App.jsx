b6a90791244fecab4a72e2f1cce0e45abea28ba3
quanghuy-811 <anhthu157.do@gmail.com>
2024-12-15 23:40:53 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "flowbite-react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <Button color="dark">button</Button>
+    </>
+  );
+}
+
+export default App;
